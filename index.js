89f3fda7477d7477618d89a4957a152abb35f16b
Juan Alorro <jralorro93@gmail.com>
2018-12-20 19:57:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,10 +17,8 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  var i = 0;
-  while (i < cart.length) {
-    if (cart.length === 1) {
-      return `In your cart, you have ${cart[i]}`;
+    if (cart.length === 0) {
+      return "Your shopping cart is empty.";
       } 
       else 
         if (cart.length === 2) {
