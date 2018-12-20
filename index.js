c2e924adb8ce771031cefae7a4db71f965e5cb06
Juan Alorro <jralorro93@gmail.com>
2018-12-20 20:05:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,14 +24,11 @@ function viewCart() {
       return `In your cart, you have ${cart[0].itemName} at ${cart[0].itemPrice}.`;
     }
       else if (cart.length === 2) {
-        return `In your cart, you have ${cart[0.itemName]} at ${cart[0.itemPrice]}, and ${cart[1].itemName} at ${cart[1].itemPrice}`;
+        return `In your cart, you have ${cart[0].itemName} at ${cart[0].itemPrice]}, and ${cart[1].itemName} at ${cart[1].itemPrice}`;
       } 
-      else if (cart.length === 3) {
+      else {
         return `In your cart, you have ${cart[0]}. ${cart[1]}, and ${cart[2]}.`;
               }   
-      else {
-        return "Your shopping cart is empty.";
-      }
 }
 
 
